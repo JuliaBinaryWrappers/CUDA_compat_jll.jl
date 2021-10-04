026627ff4516ce891cbaa6c26d5dc334ae0b19d0
jlbuild <juliabuildbot@gmail.com>
2021-10-04 11:07:04 +0000

    CUDA_compat_jll build 0.0.1+0

---
+++ src/CUDA_compat_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule CUDA_compat_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("CUDA_compat")
+JLLWrappers.@generate_main_file("CUDA_compat", UUID("340b3129-3577-50c6-bc43-5dfc29cb9805"))
+end  # module CUDA_compat_jll
